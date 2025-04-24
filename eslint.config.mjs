e63685f050425bdfefae19107c8bee71c4dcad88
Zehrei Verj <moraleszehreiverj8@gmail.com>
2025-04-25 02:23:43 +0800

    feat: add eslint-plugin-check-file and update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import checkFile from 'eslint-plugin-check-file';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +15,91 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.extends('next', 'next/core-web-vitals', 'next/typescript'),
     eslintConfigPrettier,
+    {
+        rules: {
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+            'sort-imports': [
+                'error',
+                {
+                    ignoreCase: false,
+                    ignoreDeclarationSort: true,
+                    ignoreMemberSort: false,
+                },
+            ],
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index',
+                        'object',
+                    ],
+                    'newlines-between': 'always',
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                },
+            ],
+            'import/no-restricted-paths': [
+                'error',
+                {
+                    zones: [
+                        {
+                            target: './src/features/feature-one',
+                            from: './src/features',
+                            except: ['./feature-one'],
+                        },
+                        {
+                            target: './src/features/feature-two',
+                            from: './src/features',
+                            except: ['./feature-two'],
+                        },
+                        {
+                            target: './src/features',
+                            from: './src/app',
+                        },
+                        {
+                            target: [
+                                './src/components',
+                                './src/hooks',
+                                './src/lib',
+                                './src/types',
+                                './src/utils',
+                            ],
+                            from: ['./src/features', './src/app'],
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+    {
+        files: ['src/**/*.*'],
+        plugins: {
+            'check-file': checkFile,
+        },
+        rules: {
+            'check-file/no-index': 'error',
+            'check-file/filename-naming-convention': [
+                'error',
+                {
+                    '**/*.{ts,tsx}': 'KEBAB_CASE',
+                },
+                {
+                    ignoreMiddleExtensions: true,
+                },
+            ],
+            'check-file/folder-naming-convention': [
+                'error',
+                {
+                    '!(src/app)/**/*': 'KEBAB_CASE',
+                    '!(**/__tests__)/**/*': 'KEBAB_CASE',
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
