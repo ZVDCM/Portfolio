75b7e306161edbd63bfdfbdde04229fefcc2c61d
Zehrei Verj <moraleszehreiverj8@gmail.com>
2025-04-25 14:25:35 +0800

    feat: update lint script to include auto-fix option

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,7 @@
 import path from 'path';
 
 const buildEslintCommand = (filenames) =>
-    `next lint --fix --file ${filenames
-        .map((f) => path.relative(process.cwd(), f))
-        .join(' --file ')}`;
+    `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
 const config = {
     '*.{js,jsx,mjs,ts,tsx}': ['prettier --write', buildEslintCommand],
