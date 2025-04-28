d3ccd81f4ced0216e271ab413c01bb12561c933f
Zehrei Verj <moraleszehreiverj8@gmail.com>
2025-04-28 13:11:28 +0800

    feat: add middleware for Content Security Policy handling

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+export function middleware(request: NextRequest) {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+
+    const cspHeader = `
+        default-src 'self';
+        connect-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${NODE_ENV === 'development' ? "'unsafe-eval'" : ''};
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' blob: data:;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        upgrade-insecure-requests;
+    `;
+
+    const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
+
+    const requestHeaders = new Headers(request.headers);
+    requestHeaders.set('x-nonce', nonce);
+
+    requestHeaders.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
+    const response = NextResponse.next({
+        request: {
+            headers: requestHeaders,
+        },
+    });
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+
+    return response;
+}
+
+export const config = {
+    matcher: [
+        {
+            source: '/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt).*)',
+            missing: [
+                { type: 'header', key: 'next-router-prefetch' },
+                { type: 'header', key: 'purpose', value: 'prefetch' },
+            ],
+        },
+    ],
+};
