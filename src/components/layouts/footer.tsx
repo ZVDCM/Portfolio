e8d01e4e1a9d54a57fade941c4137299b64abfbe
Zehrei Verj <moraleszehreiverj8@gmail.com>
2025-04-30 20:19:54 +0800

    fix: update social icons to use IconType for better type safety and rendering

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import React from 'react';
+import { IconType } from 'react-icons';
 import { FaGithub, FaLinkedin } from 'react-icons/fa6';
 
 import { SectionsContext } from '@/components/providers/sections-context';
@@ -11,17 +12,17 @@ import { THEME } from '@/config/theme';
 import { cn } from '@/lib/utils/cn';
 
 interface ISocials {
-    icon: React.ReactNode;
+    icon: IconType;
     href: string;
 }
 
 const SOCIALS: ISocials[] = [
     {
-        icon: <FaLinkedin />,
+        icon: FaLinkedin,
         href: '#',
     },
     {
-        icon: <FaGithub />,
+        icon: FaGithub,
         href: '#',
     },
 ];
@@ -32,13 +33,13 @@ export default function Footer({ children }: React.PropsWithChildren) {
 
     return (
         <footer className="border-t">
-            <div className="container flex gap-4 px-0">
-                <div className="flex flex-1 flex-col gap-8 p-8">
+            <div className="container flex px-0">
+                <div className="flex flex-1 flex-col gap-8 p-8 pr-0">
                     <Link href="/" className="w-fit font-mono text-3xl font-bold">
                         ZVM
                     </Link>
                     <div className="flex">
-                        <div className="flex basis-full flex-col items-start gap-8">
+                        <div className="flex flex-1 flex-col items-start gap-8">
                             <p className="w-[50ch] text-sm">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex
                                 exercitationem ut optio illum eos doloremque vitae id minus non
@@ -46,23 +47,29 @@ export default function Footer({ children }: React.PropsWithChildren) {
                                 unde.
                             </p>
                             <div className="flex justify-between gap-4">
-                                {SOCIALS.map((social, index) => (
-                                    <Link key={`social-${index}`} href={social.href}>
-                                        {social.icon}
+                                {SOCIALS.map((social, index) => {
+                                    const Icon = social.icon;
+
+                                    return (
+                                        <Link key={`social-${index}`} href={social.href}>
+                                            <Icon size="1.2rem" />
+                                        </Link>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div className="flex flex-1 justify-center">
+                            <div className="flex flex-col gap-2">
+                                {sectionIds.map((id) => (
+                                    <Link
+                                        key={id}
+                                        href={`#${id}`}
+                                        className="font-mono text-xs font-light underline underline-offset-4"
+                                    >
+                                        {id}
                                     </Link>
                                 ))}
                             </div>
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            {sectionIds.map((id) => (
-                                <Link
-                                    key={id}
-                                    href={`#${id}`}
-                                    className="font-mono text-xs font-light underline underline-offset-4"
-                                >
-                                    {id}
-                                </Link>
-                            ))}
                         </div>
                     </div>
 
