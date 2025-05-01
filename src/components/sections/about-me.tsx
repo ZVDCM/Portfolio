790f01a71e995819b39613dab989e99bbc62830e
Zehrei Verj <moraleszehreiverj8@gmail.com>
2025-05-01 17:36:54 +0800

    refactor: restructure About Me section for improved semantics and layout

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -9,38 +9,41 @@ import Socials from '@/components/socials';
 
 export default function AboutMe() {
     return (
-        <div className="container flex h-screen px-0">
-            <div className="flex-1">
-                <Image
-                    priority
-                    src={me}
-                    alt={"It's me"}
-                    placeholder="blur"
-                    className="size-full object-cover"
-                />
-            </div>
-            <div className="relative flex flex-1 flex-col justify-center">
-                <div className="flex flex-col gap-8 pl-16">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-foreground font-mono text-4xl font-bold">
-                            Zehrei Verj Morales
-                        </h1>
-                        <p className="font-mono text-xl">Full-stack Developer</p>
-                    </div>
-                    <p className="w-[60ch] text-sm">
-                        I build cross-platform apps that blend design and technology. My mission is
-                        to deliver beautiful, high-performance solutions by combining full-stack
-                        development, artistic vision, and DevOps expertise
-                    </p>
-                    <div className="flex items-center gap-4">
-                        <DownloadResume />
-                        <Socials />
-                    </div>
+        <>
+            <h2 className="sr-only">About Me</h2>
+            <div className="container flex h-screen px-0">
+                <figure className="flex-1">
+                    <Image
+                        priority
+                        src={me}
+                        alt={"It's me"}
+                        placeholder="blur"
+                        className="size-full object-cover"
+                    />
+                </figure>
+                <div className="relative flex flex-1 flex-col justify-center">
+                    <article className="flex flex-col gap-8 pl-16">
+                        <div className="flex flex-col gap-2">
+                            <h1 className="text-foreground font-mono text-4xl font-bold">
+                                Zehrei Verj Morales
+                            </h1>
+                            <p className="font-mono text-xl">Full-stack Developer</p>
+                        </div>
+                        <p className="w-[60ch] text-sm">
+                            I build cross-platform apps that blend design and technology. My mission
+                            is to deliver beautiful, high-performance solutions by combining
+                            full-stack development, artistic vision, and DevOps expertise
+                        </p>
+                        <div className="flex items-center gap-4">
+                            <DownloadResume />
+                            <Socials />
+                        </div>
+                    </article>
+                    <span className="absolute bottom-16 left-16 font-mono text-xs font-light">
+                        scroll-down
+                    </span>
                 </div>
-                <span className="absolute bottom-16 left-16 font-mono text-xs font-light">
-                    scroll-down
-                </span>
             </div>
-        </div>
+        </>
     );
 }
